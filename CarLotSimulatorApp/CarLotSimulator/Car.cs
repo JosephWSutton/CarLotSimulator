e98de1fe5513850d6805b07c73453700a18d89a7
Joseph Sutton <jsutton1000@gmail.com>
2020-11-03 14:59:50 -0600

    completed the exercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public bool IsDriveable { get; set; }
+        public string HonkNoise { get; set; }
+        public string EngineNoise { get; set; }
+
+
+        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDriveable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDriveable;
+        }
+
+        public void MakeHonkNoise(string honk)
+        {
+            Console.WriteLine(honk);
+        }
+
+        public void MakeEngineNoise(string engine)
+        {
+            Console.WriteLine(engine);
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -13,7 +13,46 @@ namespace CarLotSimulator
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
 
+            var Lot = new CarLot();
 
+            var gWagon = new Car();
+            gWagon.Year = 2020;
+            gWagon.Make = "Mercedes";
+            gWagon.Model = "G-Class";
+            gWagon.EngineNoise = "haavuuuuuuuuuum";
+            gWagon.HonkNoise = "heaaaaaank!";
+            gWagon.IsDriveable = true;
+
+            Lot.Cars.Add(gWagon);
+
+            var camaro = new Car()
+            {
+                Make = "Chevrolet",
+                Model = "Camaro ss",
+                Year = 1968,
+                EngineNoise = "vruuuum",
+                HonkNoise = "Beeeeep",
+                IsDriveable = false
+            };
+
+            Lot.Cars.Add(camaro);
+
+            var wrx = new Car(2004, "Subaru", "WRXSTI", "spheeeww", "hiank", true);
+
+            Lot.Cars.Add(wrx);
+
+            gWagon.MakeEngineNoise(gWagon.EngineNoise);
+            camaro.MakeEngineNoise(camaro.EngineNoise);
+            wrx.MakeEngineNoise(wrx.EngineNoise);
+
+            gWagon.MakeHonkNoise(gWagon.HonkNoise);
+            camaro.MakeHonkNoise(camaro.HonkNoise);
+            wrx.MakeHonkNoise(wrx.HonkNoise);
+
+            foreach(var car in Lot.Cars)
+            {
+                Console.WriteLine($"{car.Year} {car.Make} {car.Model}");
+            }
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
